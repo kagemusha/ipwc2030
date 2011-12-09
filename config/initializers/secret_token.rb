3e45fed2ecaa9c361cb46773151825c7f2789c32
Michael Madrid <mmadrid99@gmail.com>
2011-12-09 12:47:41 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ipwc2030::Application.config.secret_token = '5249b158174525ae514fb76f83542bbbf7e0cbfbc4f0fa2e7c19fd0251dcae473762800b92c4f90bd50bcd619d4be30e1773f08b68ed33511591ad79a38370c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ipwc2030::Application.config.session_store :cookie_store, :key => '_ipwc2030_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ipwc2030::Application.config.session_store :active_record_store
